51fe000c5a5b04e9b3c0aeece3cee041dc393327
COD5689 <119950881+COD5689@users.noreply.github.com>
2023-08-03 22:50:36 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAtSqjnx7KzpGl9APBPDXEOvM7AOAUCGUo",
+    authDomain: "lets-chat-app-bc4c3.firebaseapp.com",
+    databaseURL: "https://lets-chat-app-bc4c3-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-app-bc4c3",
+    storageBucket: "lets-chat-app-bc4c3.appspot.com",
+    messagingSenderId: "632070989271",
+    appId: "1:632070989271:web:179f1aa3607d231abdd792",
+    measurementId: "G-1ZXNGN1ZWE"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
